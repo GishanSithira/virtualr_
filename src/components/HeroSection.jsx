a4032a11318bb51c5b4a2407084108e7bcd166e1
GishanSithira <dinethspace@gmail.com>
2024-12-22 22:50:28 +0530

    changed to GSPixel

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -5,10 +5,10 @@ const HeroSection = () => {
   return (
     <div className="flex flex-col items-center mt-6 lg:mt-20">
       <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide ">
-        VirtualR build tools
+        Welcome To
         <span className="bg-gradient-to-r from-orange-500 to-orange-800 text-transparent bg-clip-text">
           {" "}
-          for Gishan
+          GSPixel
         </span>
       </h1>
       <p className="mt-10 text-lg text-center text-neutral-500 max-w-4xl">
